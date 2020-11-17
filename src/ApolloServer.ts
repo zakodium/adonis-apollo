d751da8871dd6cd7f8b895e86904d669418cff6d
Daniel Kostro <kostro.d@gmail.com>
2020-11-17 16:16:42 +0100

    use version ^1.7.0 of react playground cdn build

--- src/ApolloServer.ts
+++ src/ApolloServer.ts
@@ -97,7 +97,7 @@ export default class ApolloServer extends ApolloServerBase {
     return async (ctx: HttpContextContract) => {
       const playgroundOptions = createPlaygroundOptions({
         endpoint: this.$path,
-        version: '',
+        version: '^1.7.0',
         settings: {
           'request.credentials': 'include',
           ...this.$config.playgroundSettings,
