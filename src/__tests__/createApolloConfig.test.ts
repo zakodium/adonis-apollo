31d2710f32c53bf5b3345ec17de49d79566aef34
Daniel Kostro <kostro.d@gmail.com>
2024-01-09 13:47:24 +0100

    tests: test config with Ioc namespace

--- src/__tests__/createApolloConfig.test.ts
+++ src/__tests__/createApolloConfig.test.ts
@@ -4,6 +4,15 @@ import createApolloConfig from '../createApolloConfig';
 
 const testIoC = new Ioc();
 
+class Formatter {
+  formatError() {
+    return {
+      message: 'error',
+    };
+  }
+}
+testIoC.singleton('App/Exceptions/GraphqlHandler', () => new Formatter());
+
 describe('apollo config', () => {
   it('create apollo configuration, formatError is callback', () => {
     const config = createApolloConfig(
@@ -14,6 +23,33 @@ describe('apollo config', () => {
         apolloServer: {
           introspection: true,
           formatError: () => ({ message: 'error' }),
+        },
+      },
+      {
+        ioc: testIoC,
+        fallbackUrl: 'http://localhost:3333',
+      },
+    );
+    expect(config).toEqual({
+      schemas: 'app/Schemas',
+      resolvers: 'app/Resolvers',
+      path: '/graphql',
+      apolloServer: {
+        introspection: true,
+        formatError: expect.any(Function),
+      },
+    });
+  });
+
+  it('create apollo configuration, formatError is an IoC dependency', () => {
+    const config = createApolloConfig(
+      {
+        schemas: 'app/Schemas',
+        resolvers: 'app/Resolvers',
+        path: '/graphql',
+        apolloServer: {
+          introspection: true,
+          formatError: 'App/Exceptions/GraphqlHandler',
         },
       },
       {
